cc0d9a4010e7388c2ced917509c6f52f9d38c248
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 16:05:58 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -16,6 +16,16 @@ class GifListContainer extends React.Component {
       <GifList gifs={this.state.gifs} />;
     )
   }
+
+  fetchGIFS = (query = "dolphins") => {
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        gifs: data
+      })
+    })
+  }
 }
 
 export default GifListContainer
